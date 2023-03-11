443b89cba2bf557d90fccaf24d0cb9127bd01cbe
Saurabh Gawade <saurabhgawade2920@gmail.com>
2023-03-11 21:18:19 +0530

    Basic Footer Update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,9 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="fixed-bottom bg-dark text-white">
+    <div className="fixed-bottom bg-light">
       <div className="container">
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-1">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-1 border-top">
           <div className="col-md-4 d-flex align-items-center">
             MINI Projects - MADE WITH ❤
           </div>
