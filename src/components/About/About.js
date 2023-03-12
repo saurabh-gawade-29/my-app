30536763354cf3389582edfe4f284ef405afbf55
Saurabh Gawade <saurabhgawade2920@gmail.com>
2023-03-12 15:27:51 +0530

    bug fix

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,13 +4,13 @@ import "./About.css";
 const About = () => {
   return (
     <div>
-      <div class="card text-bg-light mb-3 shadow">
-        <div class="card-header display-6">About Us</div>
-        <div class="card-body">
-          <h5 class="card-title">
+      <div className="card text-bg-light mb-3 shadow">
+        <div className="card-header display-6">About Us</div>
+        <div className="card-body">
+          <h5 className="card-title">
             These Are Mini Projects For Educational Purpose
           </h5>
-          <p class="card-text">Some quick example of API Integration</p>
+          <p className="card-text">Some quick example of API Integration</p>
           <ol>
             <li>ASTRO: https://go-apod.herokuapp.com/apod</li>
             <li>USER: https://randomuser.me/api/</li>
