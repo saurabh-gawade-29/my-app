3016e24a412b851ce9129445d4c29ea619093ae5
Saurabh Gawade <saurabhgawade2920@gmail.com>
2023-03-12 14:37:52 +0530

    Dog Page Done

--- src/components/Dog/Dog.js
+++ src/components/Dog/Dog.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./Dog.scss";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const baseURL = "https://random.dog/woof.json";
 
@@ -8,8 +9,11 @@ const Dog = () => {
   const [post, setPost] = useState();
   const [img, setImg] = useState();
   const [fileSize, setFileSize] = useState();
+  const [loading, setLoading] = useState(true);
+  const [color, setColor] = useState("#e84118");
 
-  useEffect(() => {
+  const getDog = () => {
+    setLoading(true);
     axios
       .get(baseURL)
       .then((response) => {
@@ -17,22 +21,52 @@ const Dog = () => {
         let POST = response.data;
         setImg(POST.url);
         setFileSize(POST.fileSizeBytes);
+        setLoading(false);
       })
       .catch((error) => console.error(error));
+  };
+  useEffect(() => {
+    getDog();
   }, []);
 
   return (
     <div>
-      <div className="wrap-dog d-flex justify-content-center">
-        <div className="card text-bg-dark">
-          <img src={img} className="card-img " alt="DOG IMAGE IS HERE" />
-          <div className="card-img-overlay">
-            <p className="card-text text-danger">
-              <small>FileSize: {fileSize} Bytes</small>
-            </p>
+      {loading === true ? (
+        <>
+          <div className="myLoader">
+            <ClipLoader
+              className="myLoader"
+              color={color}
+              loading={loading}
+              size={150}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <>
+          <div className="wrap-dog">
+            <div className="card text-bg-dark my-dog-card">
+              <img
+                src={img}
+                className="card-img my-dog-img"
+                alt="DOG IMAGE IS HERE"
+              />
+              <div className="card-img-overlay">
+                <p className="card-text text-danger">
+                  <small>FileSize: {fileSize} Bytes</small>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="text-center my-4">
+            <button type="button" class="btn mat-balck-btn" onClick={getDog}>
+              Next
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
