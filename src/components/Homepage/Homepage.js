d4b8e9530eed7c17a38b7e04e7b961cabf0126f8
saurabh gawade <saurabhgawade2920@gmail.com>
2023-03-13 19:00:54 +0530

    btn changes

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -5,60 +5,69 @@ import fake from "../images/fake.jpg";
 import joke from "../images/Joke.jpg";
 import dog from "../images/Dog.jpg";
 import { Link } from "react-router-dom";
+import "./Homepage.scss";
 
 const Homepage = () => {
   return (
     <div>
       <div className="row">
-        <div className="col-12 col-sm-6 col-md-4 col-lg-4">
+        <div className="col-12 col-sm-6 col-md-4 col-lg-4 mb-2">
           <div className="card shadow">
             <img src={astro} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title text-truncate">Galaxy</h5>
+              <h5 className="card-title text-truncate home-card-link">
+                Galaxy
+              </h5>
               <p className="card-text">
                 Contains Information About Space & Galaxy.
               </p>
-              <Link to="/astro" className="btn mat-balck-btn ">
+              <Link to="/astro" className="btn explore-btn ">
                 Explore
               </Link>
             </div>
           </div>
         </div>
-        <div className="col-12 col-sm-6 col-md-4 col-lg-4">
+        <div className="col-12 col-sm-6 col-md-4 col-lg-4 mb-2">
           <div className="card shadow">
             <img src={fake} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title text-truncate">Generate Random User</h5>
+              <h5 className="card-title text-truncate home-card-link">
+                Generate Random User
+              </h5>
               <p className="card-text">
                 Contains Information About Random Users.
               </p>
-              <Link to="/user" className="btn mat-balck-btn ">
+              <Link to="/user" className="btn explore-btn ">
                 Explore
               </Link>
             </div>
           </div>
         </div>
-        <div className="col-12 col-sm-6 col-md-4 col-lg-4">
+        <div className="col-12 col-sm-6 col-md-4 col-lg-4 mb-2">
           <div className="card shadow">
             <img src={joke} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title text-truncate">Daily Jokes</h5>
+              <h5 className="card-title text-truncate home-card-link">
+                Daily Jokes
+              </h5>
               <p className="card-text">Daily Laughter Dose</p>
-              <Link to="/joke" className="btn mat-balck-btn ">
+              <Link to="/joke" className="btn explore-btn ">
                 Explore
               </Link>
             </div>
           </div>
         </div>
       </div>
-      <div className="row my-2">
-        <div className="col-12 col-sm-6 col-md-4 col-lg-4">
+      <div className="row">
+        <div className="col-12 col-sm-6 col-md-4 col-lg-4 mb-2">
           <div className="card shadow">
             <img src={dog} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title text-truncate">Dog Images</h5>
+              <h5 className="card-title text-truncate home-card-link">
+                Dog Images
+              </h5>
               <p className="card-text">Woof Woof! See My Pictures</p>
-              <Link to="/dog" className="btn mat-balck-btn ">
+              <Link to="/dog" className="btn explore-btn ">
                 Explore
               </Link>
             </div>
